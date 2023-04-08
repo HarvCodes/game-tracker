23b265aa89d86d3998bc6633843278e60a792bc9
HarvCodes <mrharv@gmail.com>
2023-04-08 23:06:09 +0100

    Updated index.html with Firebase configuration

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
