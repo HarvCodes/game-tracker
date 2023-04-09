55324a4581aa0cc76a7c63757b4f7043afb9c5d9
HarvCodes <mrharv@gmail.com>
2023-04-09 15:15:54 +0100

    Updated Firebase configuration and app.js import statements

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAWjpIJ5EBOpHNykAc-7YZXEJA_55ipw0Q",
-  authDomain: "harvs-game-tracker.firebaseapp.com",
-  projectId: "harvs-game-tracker",
-  storageBucket: "harvs-game-tracker.appspot.com",
-  messagingSenderId: "118470639156",
-  appId: "1:118470639156:web:2351a582685022ce769461",
-  measurementId: "G-NWF5PMQ7XH",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
-const db = getFirestore();
-
 document.addEventListener("DOMContentLoaded", function () {
   toggleDarkMode();
   handleAuthentication();
@@ -23,17 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Toggle dark mode
 function toggleDarkMode() {
-  const toggleButton = document.getElementById("dark-mode-toggle");
-  const body = document.querySelector("body");
-
-  toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-  });
+  const body = document.body;
+  body.classList.toggle("dark-mode");
 }
 
 // Handle user authentication
 function handleAuthentication() {
-  onAuthStateChanged(auth, (user) => {
+  firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, show the logout button
       document.getElementById("logout-btn").hidden = false;
@@ -56,7 +34,7 @@ async function loginUser() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
     console.log("User logged in:", userCredential.user);
   } catch (error) {
     console.error("Error during login:", error);
@@ -67,7 +45,7 @@ async function registerUser() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
     console.log("User registered:", userCredential.user);
   } catch (error) {
     console.error("Error during registration:", error);
@@ -75,6 +53,6 @@ async function registerUser() {
 }
 
 async function logoutUser() {
-  await signOut(auth);
+  await firebase.auth().signOut();
   alert("Successfully logged out!");
 }
